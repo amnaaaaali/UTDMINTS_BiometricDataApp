784b8642066474bd48ede2481e5a0af04a36447c
Amna Ali <42101696+amnaaaaali@users.noreply.github.com>
2019-11-15 10:21:26 -0600

    Updating Z_Scores_PlotLines.py (adding comments)

--- Z_Scores_PlotLines.py
+++ Z_Scores_PlotLines.py
@@ -5,9 +5,15 @@ from EEGArray import EEGArray
 
 # Create a method to plot red lines
 def Red_Lines(e1, e2, ax):
-    ax.plot(np.linspace(e1[0], e2[0], 5), np.linspace(e1[1], e2[1], 5), 'r-')
+    # define properties of red line
+    line1,  = ax.plot(np.linspace(e1[0], e2[0], 5), np.linspace(e1[1], e2[1], 5), 'r-')
+    # return line1
+    return line1
 
 
 # Create a method to plot blue lines
 def Blue_Lines(e1, e2, ax):
-    ax.plot(np.linspace(e1[0], e2[0], 5), np.linspace(e1[1], e2[1], 5), 'b-')
+    # define properties of blue line
+    line2, = ax.plot(np.linspace(e1[0], e2[0], 5), np.linspace(e1[1], e2[1], 5), 'b-')
+    # return line2
+    return line2
