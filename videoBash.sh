c322a12d13d18230aff9926b8b0290c9412f0eb0
Ritika Shrivastava <ritika.shrivastava@utdallas.edu>
2019-12-10 02:17:54 -0600

    Commented videoBash.py

--- videoBash.sh
+++ videoBash.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+#created a local variable 'a' and set the path for the python that the Tobii video streaming uses.
+#This can be changed in the future depending on the OS as needed.
+#we just reference the a variable and use it as a command to run the tobii_gui python file. 
 a='C:/msys64/mingw64/bin/python.exe'
 $a tobii_gui.py
